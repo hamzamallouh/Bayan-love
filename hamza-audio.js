662d6e2ad574c3d6cafe1b463c9e553991a6b88c
hamzamallouh <89353073+hamzamallouh@users.noreply.github.com>
2026-08-20 07:05:47 +0300

    Update Hamza popup text and keep Hamza song

--- hamza-audio.js
+++ hamza-audio.js
@@ -4,8 +4,12 @@
   window.showHamza=()=>{
     if(audio){audio.src=encodeURI(SONG);audio.loop=true;audio.load();audio.play().catch(()=>{});}
     let box=document.getElementById('hamzaLovePopup');
-    if(!box){box=document.createElement('div');box.id='hamzaLovePopup';box.innerHTML='<div class="hamzaLoveInner">بحبك بيان بحبك ❤️</div>';document.body.appendChild(box);}
+    if(!box){
+      box=document.createElement('div');box.id='hamzaLovePopup';
+      box.innerHTML='<div class="hamzaLoveInner">الاشي الوحيد الي بقلب حمزة هو انت<br><br><span>💚بحبك بيان💚</span><br><span>🩷بحبك بيون🩷</span><br><span>❤️بحبك بيونتي❤️</span></div>';
+      document.body.appendChild(box);
+    }
     box.classList.remove('show');void box.offsetWidth;box.classList.add('show');
-    setTimeout(()=>box.classList.remove('show'),6000);
+    setTimeout(()=>box.classList.remove('show'),8000);
   };
 })();
